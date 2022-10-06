fcd810b58f3b2de82a7761abee2e7c8da895f965
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-10-06 19:37:41 -0400

    logo link to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import logo from "../logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-main">
-        <img src={logo} alt="logo" />
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
         <h2>ENook</h2>
       </div>
       <h3>welcome</h3>
