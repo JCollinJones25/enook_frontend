5c8228e3105f96a9c6ef91ffd5b743d7357e9d5f
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-10-06 20:19:47 -0400

    make fetch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,11 +4,32 @@ import Library from "../pages/Library";
 import Show from "../pages/Show";
 
 const Main = () => {
+  const [books, setBooks] = useState([]);
+
+  const url = "https://enook-api.herokuapp.com/library";
+
+  const getBooks = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => setBooks(result));
+  };
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  console.log(books);
+
   return (
     <main>
       <Routes>
         <Route path="/" element={<Library />} />
-        <Route path="/library/:id" render={(rp) => <Show {...rp} />} />
+        <Route
+          path="/library/:id"
+          render={(rp) => {
+            <Show {...rp} />;
+          }}
+        />
       </Routes>
     </main>
   );
