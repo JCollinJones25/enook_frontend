197fca1db4cfd8e317eead0098d15e71026bb295
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-10-07 17:20:02 -0400

    sass file structure and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import "./sass/main.scss";
+import "./sass/sidebar.scss";
+import "./sass/library.scss";
+import "./sass/show.scss";
+import "./sass/spinner.scss";
 
 function App() {
   return (
