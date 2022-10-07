26cb0758846df7b768d149af5afceb7de56aea1a
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-10-07 16:45:31 -0400

    style sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import { Twirl as Hamburger } from "hamburger-react";
 
 const Sidebar = () => {
+
+    const [sidebar, setSidebar] = useState(false)
+
+    const showSidebar = () => {
+        setSidebar(!sidebar)
+    }
+
     return (
-        <div className="sidebar">
-            <Hamburger />
-        </div>
+        <div className="hamburger">
+        <Hamburger 
+          size={25}
+          onClick={showSidebar}
+          toggled={sidebar}
+          toggle={setSidebar}>
+        </Hamburger>
+        <nav className={sidebar ? "sidebar active" : "sidebar"}>
+          <ul className="sidebar-items">
+            <li className="sidebar-toggle">
+              <Hamburger
+                size={25}
+                onClick={showSidebar}
+                toggled={sidebar}
+                toggle={setSidebar}
+              ></Hamburger>
+            </li>
+          </ul>
+        </nav>
+      </div>
     )
 }
 
