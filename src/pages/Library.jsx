c1a1dfb533e473ae0c1e5f9381ac1e19c1586d5a
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-10-10 16:46:43 -0400

    add ternary for conditional render library

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,28 +1,40 @@
 import { Link } from "react-router-dom";
 
 const Library = ({ books }) => {
-  return (
-    <>
-      <h3>Library</h3>
-      <hr></hr>
-      <div className="library">
-        {books.map((book) => {
-          return (
-            <div key={book._id} className="book">
-              <Link to={`/${book._id}`}>
-                <img src={book.cover} alt={book.title} />
-                <div>
-                  <h3>{book.title}</h3>
-                  <p>by {book.author}</p>
-                  <h4 id="price">${book.price}</h4>
-                </div>
-              </Link>
-            </div>
-          );
-        })}
+  const loaded = () => {
+    return (
+      <>
+        <h3>Library</h3>
+        <hr></hr>
+        <div className="library">
+          {books.map((book) => {
+            return (
+              <div key={book._id} className="book">
+                <Link to={`/${book._id}`}>
+                  <img src={book.cover} alt={book.title} />
+                  <div>
+                    <h3>{book.title}</h3>
+                    <p>by {book.author}</p>
+                    <h4 id="price">${book.price}</h4>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  };
+
+  const loading = () => {
+    return (
+      <div className="spinner-container">
+        <div className="spinner"></div>
       </div>
-    </>
-  );
+    );
+  };
+
+  return books ? loaded() : loading();
 };
 
 export default Library;
