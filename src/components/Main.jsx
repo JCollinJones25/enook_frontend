985b9ffaba85efdd4769fee2ca9c328603291067
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-10-10 22:19:04 -0400

    ternary working for library

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,12 +10,12 @@ const Main = () => {
 
   const getBooks = async () => {
     try {
-      const response = await fetch(url)
-      const data = await response.json()
+      const response = await fetch(url);
+      const data = await response.json();
       setBooks(data);
-      console.log(books)
+      console.log(books);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
@@ -23,28 +23,14 @@ const Main = () => {
     getBooks();
   }, []);
 
-  const loaded = () => {
-    return (
-      <main>
-        <Routes>
-          <Route path="/" element={<Library books={books} />} />
-          <Route path="/:id" element={<Show books={books} />} />
-        </Routes>
-      </main>
-    );
-  };
-
-  const loading = () => {
-    return (
-      <main>
-        <div className="spinner-container">
-          <div className="spinner"></div>
-        </div>
-      </main>
-    );
-  };
-
-  return books ? loaded() : loading();
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Library books={books} />} />
+        <Route path="/:id" element={<Show books={books} />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default Main;
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -6,20 +6,26 @@ const Library = ({ books }) => {
       <h3>Library</h3>
       <hr></hr>
       <div className="library">
-        {books.map((book) => {
-          return (
-            <div key={book._id} className="book">
-              <Link to={`/${book._id}`}>
-                <img src={book.cover} alt={book.title} />
-                <div>
-                  <h3>{book.title}</h3>
-                  <p>by {book.author}</p>
-                  <h4 id="price">${book.price}</h4>
-                </div>
-              </Link>
-            </div>
-          );
-        })}
+        {books.length > 0 ? (
+          books.map((book) => {
+            return (
+              <div key={book._id} className="book">
+                <Link to={`/${book._id}`}>
+                  <img src={book.cover} alt={book.title} />
+                  <div>
+                    <h3>{book.title}</h3>
+                    <p>by {book.author}</p>
+                    <h4 id="price">${book.price}</h4>
+                  </div>
+                </Link>
+              </div>
+            );
+          })
+        ) : (
+          <div className="spinner-container">
+            <div className="spinner"></div>
+          </div>
+        )}
       </div>
     </>
   );
