9783d58a29151b3429eba40815fec8fcedc6c4ff
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-11-10 15:59:38 -0500

    add github link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 const Footer = () => {
   return (
-    <div className="footer">
-      <footer></footer>
-    </div>
-  );
+    <footer>
+      <a href="https://github.com/JCollinJones25/enook_frontend" target="_blank" rel="noopener noreferrer">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="github logo"/>
+      </a>
+    </footer>
+  )
 };
 
 export default Footer;
